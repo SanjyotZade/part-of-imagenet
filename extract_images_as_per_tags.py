689c0dd02babb316b660ca2f2799e674283193db
sanjyotzade <zade.sanjyot@gmail.com>
2019-09-04 16:32:39 +0530

    changing variable to with_annotation from only

--- extract_images_as_per_tags.py
+++ extract_images_as_per_tags.py
@@ -19,14 +19,14 @@ if __name__ == '__main__':
     optional.add_argument('-url', '--url_data', default=url_data_path, help='path to image-net urls data')
     optional.add_argument('-nc', '--ncode_data', default=ncodes_data_path, help='path to modified ncodes data')
 
-    optional.add_argument('-only', '--only', default=True, help='downloads images with annotations only')
+    optional.add_argument('-only', '--with_annotation', default=True, help='downloads images with annotations only')
     optional.add_argument('-p', '--parallel', default=True, help='downloaded parallelly or sequentially')
     optional.add_argument('-v', '--verbose', default=True , help='bool represent whether to display ncode level download stats')
     optional.add_argument('-b', '--batch', default=None, help='number of images to download paralelly, if parallel is TRUE')
 
     args = parser.parse_args()
     args.parallel = eval(str(args.parallel))
-    args.only = eval(str(args.only))
+    args.with_annotation = eval(str(args.with_annotation))
     args.verbose = eval(str(args.verbose))
     args.batch = eval(str(args.batch))
 
@@ -37,7 +37,7 @@ if __name__ == '__main__':
         path_to_url_dataset=args.url_data,
         path_to_annotations=args.annotations_dir,
         path_to_save_dataset=args.save_dir,
-        only_annotations=args.only,
+        only_annotations=args.with_annotation,
         parallel=args.parallel,
         verbose=args.verbose,
         batch_size_=args.batch
