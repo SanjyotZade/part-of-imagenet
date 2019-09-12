0e206d7021414d10dff63533be266bd9f69d6525
Sanjyot22 <zade.sanjyot@gmail.com>
2019-09-13 04:44:47 +0530

    renamed download prerequisites file

---
+++ download_imagenet_prerequisites.py
@@ -0,0 +1,25 @@
+# Copyright (C) 31/08/19 sanjyotzade
+import os
+import argparse
+from utility import Utils
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Downloading ncodes for imagenet dataset')
+
+    dir_path = os.path.realpath(os.path.dirname(__file__))
+    path_to_save = os.path.realpath(os.path.dirname(__file__))
+
+    optional = parser.add_argument_group('optional arguments')
+    optional.add_argument('-nc_dir', '--ncodes_dir', default=path_to_save, help='path to saved ncodes data')
+    optional.add_argument('-url_data', '--with_url_data', default=False, help='tag whether to download url data')
+
+    # downloading
+    args = parser.parse_args()
+    util_obj = Utils()
+    util_obj.download_ncodes_image_net(path_to_save=args.ncodes_dir)
+
+    args.url_data = eval(str(args.with_url_data))
+
+    # if args.url_data:
+    # use this to download imagenet urls
+    util_obj.download_image_net_urls(path_to_save=args.ncodes_dir)
